384055ae5f31847d4ec7e7d035857ff68b4a9627
anasalazmeh <anoosalazmeh@gmail.com>
2023-12-20 21:34:09 +0300

    setup modal product and image

---
+++ prisma/migrations/20231220183334_add_all_modals/migration.sql
@@ -0,0 +1,116 @@
+-- CreateTable
+CREATE TABLE "Store" (
+    "id" STRING NOT NULL,
+    "name" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Store_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Billboards" (
+    "id" STRING NOT NULL,
+    "label" STRING NOT NULL,
+    "imageUrl" STRING NOT NULL,
+    "storeId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Billboards_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" STRING NOT NULL,
+    "storeId" STRING NOT NULL,
+    "billboardId" STRING NOT NULL,
+    "name" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Size" (
+    "id" STRING NOT NULL,
+    "storeId" STRING NOT NULL,
+    "name" STRING NOT NULL,
+    "value" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Size_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Color" (
+    "id" STRING NOT NULL,
+    "storeId" STRING NOT NULL,
+    "name" STRING NOT NULL,
+    "value" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Color_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" STRING NOT NULL,
+    "storeId" STRING NOT NULL,
+    "categoryId" STRING NOT NULL,
+    "name" STRING NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "isFeatrued" BOOL NOT NULL DEFAULT false,
+    "isArchived" BOOL NOT NULL DEFAULT false,
+    "sizeId" STRING NOT NULL,
+    "colorId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" STRING NOT NULL,
+    "url" STRING NOT NULL,
+    "productId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Billboards_storeId_idx" ON "Billboards"("storeId");
+
+-- CreateIndex
+CREATE INDEX "Category_storeId_idx" ON "Category"("storeId");
+
+-- CreateIndex
+CREATE INDEX "Category_billboardId_idx" ON "Category"("billboardId");
+
+-- CreateIndex
+CREATE INDEX "Size_storeId_idx" ON "Size"("storeId");
+
+-- CreateIndex
+CREATE INDEX "Color_storeId_idx" ON "Color"("storeId");
+
+-- CreateIndex
+CREATE INDEX "Product_storeId_idx" ON "Product"("storeId");
+
+-- CreateIndex
+CREATE INDEX "Product_categoryId_idx" ON "Product"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "Product_sizeId_idx" ON "Product"("sizeId");
+
+-- CreateIndex
+CREATE INDEX "Product_colorId_idx" ON "Product"("colorId");
+
+-- CreateIndex
+CREATE INDEX "Image_productId_idx" ON "Image"("productId");
